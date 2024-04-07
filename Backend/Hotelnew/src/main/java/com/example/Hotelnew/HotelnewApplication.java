0edcb9f2ba1bbda28131b7dc6903c07c6d072e16
sumindaaa <sumi.amarasinghe@gmail.com>
2024-04-07 12:19:41 +1000

    First commit

---
+++ Backend/Hotelnew/src/main/java/com/example/Hotelnew/HotelnewApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hotelnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelnewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelnewApplication.class, args);
+	}
+
+}
